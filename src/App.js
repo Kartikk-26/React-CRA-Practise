4c4ae1da94821c4104a4ed39236ba0135e5affbe
Kartikjain2626 <kartikjain2626@gmail.com>
2024-10-21 21:09:09 +0530

    Day 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Register from './screens/Register'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './screens/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+]);
 
 function App() {
   return (
     <div>
-      <Register />
+      <RouterProvider router={router} />
     </div>
   )
 }
 
 export default App
+
+// right now we cannot access the variable outside that js file but if we want to access it we will use the REDUX which will store the states
